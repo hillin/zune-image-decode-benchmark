0bbb68d5ad8abfb4f286e4481ba87515104ddef6
hillin <hillinsilence@gmail.com>
2024-03-22 18:03:45 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Run with `cargo bench`.
+
+Result on i9-9900K (3.60 GHz) + 32GB RAM
+
+```
+image-rs load           time:   [2.0388 ms 2.0668 ms 2.0973 ms]
+zune-image load         time:   [1.1924 ms 1.2063 ms 1.2206 ms]
+
+image-rs render         time:   [3.0728 ms 3.1115 ms 3.1531 ms]
+zune-image render       time:   [1.7630 ms 1.7722 ms 1.7814 ms]
+```
---
+++ benches/image_rs_vs_zune_image.rs
@@ -0,0 +1,56 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use zune_core::options::DecoderOptions;
+use zune_image::image::Image;
+
+pub(crate) const JPEG_IMAGE_DATA: &[u8] = include_bytes!("../resources/lenna.jpg");
+
+fn load_image() -> Image {
+    Image::read(JPEG_IMAGE_DATA, DecoderOptions::default()).unwrap()
+}
+
+fn benchmark_decode_qoi(c: &mut Criterion) {
+    let image = load_image();
+    let data = image
+        .write_to_vec(zune_image::codecs::ImageFormat::QOI)
+        .unwrap();
+
+    c.bench_function("image-rs decode qoi", |b| {
+        b.iter(|| {
+            let _ = Image::read(&data, DecoderOptions::default()).unwrap();
+        })
+    });
+}
+
+fn benchmark_decode_jpeg(c: &mut Criterion) {
+    let image = load_image();
+    let data = image
+        .write_to_vec(zune_image::codecs::ImageFormat::JPEG)
+        .unwrap();
+
+    c.bench_function("image-rs decode jpg", |b| {
+        b.iter(|| {
+            let _ = Image::read(&data, DecoderOptions::default()).unwrap();
+        })
+    });
+}
+
+fn benchmark_decode_png(c: &mut Criterion) {
+    let image = load_image();
+    let data = image
+        .write_to_vec(zune_image::codecs::ImageFormat::PNG)
+        .unwrap();
+
+    c.bench_function("image-rs decode png", |b| {
+        b.iter(|| {
+            let _ = Image::read(&data, DecoderOptions::default()).unwrap();
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    benchmark_decode_qoi,
+    benchmark_decode_jpeg,
+    benchmark_decode_png
+);
+criterion_main!(benches);
