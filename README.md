37e98455ad2c63e06e23bd3d8598729476348927
hillin <hillinsilence@gmail.com>
2024-03-22 18:30:55 +0800

    add image-rs qoi encoding/decoding for reference

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 Run with `cargo bench`.
 
-Result on i9-9900K (3.60 GHz) + 32GB RAM
+Result on i9-9900K (3.60 GHz) + 64GB RAM
 
 ```
-encode qoi              time:   [5.8880 ms 5.9623 ms 6.0478 ms]
-encode jpeg             time:   [3.3849 ms 3.4074 ms 3.4323 ms]
-encode png              time:   [2.7700 ms 2.8185 ms 2.8748 ms]
+zune encode qoi         time:   [5.9150 ms 6.0006 ms 6.0984 ms]
+image-rs encode qoi     time:   [3.9894 ms 4.0335 ms 4.0826 ms]
+zune encode jpeg        time:   [3.3753 ms 3.3994 ms 3.4257 ms]
+zune encode png         time:   [2.7774 ms 2.8188 ms 2.8650 ms]
 
-decode qoi              time:   [2.0618 ms 2.0755 ms 2.0910 ms]
-decode jpeg             time:   [937.84 µs 946.73 µs 956.73 µs]
-decode png              time:   [828.25 µs 839.40 µs 850.99 µs]
+zune decode qoi         time:   [2.0401 ms 2.0505 ms 2.0617 ms]
+image-rs decode qoi     time:   [1.5920 ms 1.5990 ms 1.6066 ms]
+zune decode jpeg        time:   [942.27 µs 948.51 µs 955.29 µs]
+zune decode png         time:   [853.24 µs 869.38 µs 886.10 µs]
 ```
--- benches/zune_image_codec_benchmark.rs
+++ benches/zune_image_codec_benchmark.rs
@@ -1,80 +1,117 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use image::RgbImage;
 use zune_core::options::DecoderOptions;
 use zune_image::{codecs::ImageFormat, image::Image};
 
 pub(crate) const JPEG_IMAGE_DATA: &[u8] = include_bytes!("../resources/lenna.jpg");
 
-fn load_image() -> Image {
+fn zune_load_image() -> Image {
     Image::read(JPEG_IMAGE_DATA, DecoderOptions::default()).unwrap()
 }
 
-fn encode(format: ImageFormat) -> Vec<u8> {
-    let image = load_image();
+fn zune_encode(format: ImageFormat) -> Vec<u8> {
+    let image = zune_load_image();
     image.write_to_vec(format).unwrap()
 }
 
-fn decode(data: &[u8]) -> Image {
+fn zune_decode(data: &[u8]) -> Image {
     Image::read(data, DecoderOptions::default()).unwrap()
 }
 
-fn benchmark_encode_qoi(c: &mut Criterion) {
-    c.bench_function("encode qoi", |b| {
+pub fn irs_load_image() -> RgbImage {
+    image::load_from_memory(JPEG_IMAGE_DATA).unwrap().to_rgb8()
+}
+
+fn irs_encode(format: image::ImageFormat) -> Vec<u8> {
+    let image = irs_load_image();
+    let mut data = Vec::new();
+    let mut cursor = std::io::Cursor::new(&mut data);
+    image.write_to(&mut cursor, format).unwrap();
+
+    data
+}
+
+fn irs_decode(data: &[u8]) -> RgbImage {
+    image::load_from_memory(data).unwrap().to_rgb8()
+}
+
+fn benchmark_zune_encode_qoi(c: &mut Criterion) {
+    c.bench_function("zune encode qoi", |b| {
         b.iter(|| {
-            let _ = encode(ImageFormat::QOI);
+            let _ = zune_encode(ImageFormat::QOI);
         })
     });
 }
 
-fn benchmark_decode_qoi(c: &mut Criterion) {
-    let data = encode(ImageFormat::QOI);
-    c.bench_function("decode qoi", |b| {
+fn benchmark_zune_decode_qoi(c: &mut Criterion) {
+    let data = zune_encode(ImageFormat::QOI);
+    c.bench_function("zune decode qoi", |b| {
         b.iter(|| {
-            let _ = decode(&data);
+            let _ = zune_decode(&data);
+        })
+    });
+}
+
+fn benchmark_irs_encode_qoi(c: &mut Criterion) {
+    c.bench_function("image-rs encode qoi", |b| {
+        b.iter(|| {
+            let _ = irs_encode(image::ImageFormat::Qoi);
+        })
+    });
+}
+
+fn benchmark_irs_decode_qoi(c: &mut Criterion) {
+    let data = irs_encode(image::ImageFormat::Qoi);
+    c.bench_function("image-rs decode qoi", |b| {
+        b.iter(|| {
+            let _ = irs_decode(&data);
         })
     });
 }
 
 fn benchmark_encode_jpeg(c: &mut Criterion) {
-    c.bench_function("encode jpeg", |b| {
+    c.bench_function("zune encode jpeg", |b| {
         b.iter(|| {
-            let _ = encode(ImageFormat::JPEG);
+            let _ = zune_encode(ImageFormat::JPEG);
         })
     });
 }
 
 fn benchmark_decode_jpeg(c: &mut Criterion) {
-    let data = encode(ImageFormat::JPEG);
-    c.bench_function("decode jpeg", |b| {
+    let data = zune_encode(ImageFormat::JPEG);
+    c.bench_function("zune decode jpeg", |b| {
         b.iter(|| {
-            let _ = decode(&data);
+            let _ = zune_decode(&data);
         })
     });
 }
 
 fn benchmark_encode_png(c: &mut Criterion) {
-    c.bench_function("encode png", |b| {
+    c.bench_function("zune encode png", |b| {
         b.iter(|| {
-            let _ = encode(ImageFormat::PNG);
+            let _ = zune_encode(ImageFormat::PNG);
         })
     });
 }
 
 fn benchmark_decode_png(c: &mut Criterion) {
-    let data = encode(ImageFormat::PNG);
-    c.bench_function("decode png", |b| {
+    let data = zune_encode(ImageFormat::PNG);
+    c.bench_function("zune decode png", |b| {
         b.iter(|| {
-            let _ = decode(&data);
+            let _ = zune_decode(&data);
         })
     });
 }
 
 criterion_group!(
     benches,
-    benchmark_encode_qoi,
+    benchmark_zune_encode_qoi,
+    benchmark_irs_encode_qoi,
     benchmark_encode_jpeg,
     benchmark_encode_png,
-    benchmark_decode_qoi,
+    benchmark_zune_decode_qoi,
+    benchmark_irs_decode_qoi,
     benchmark_decode_jpeg,
-    benchmark_decode_png
+    benchmark_decode_png,
 );
 criterion_main!(benches);
